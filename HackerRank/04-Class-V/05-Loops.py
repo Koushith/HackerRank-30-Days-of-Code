df166258c639234a087dc490dc5e2a5fe3d23596
unknown <koushith97@gmail.com>
2019-07-21 11:44:55 +0530

    day4

--- HackerRank/04-Class-V/05-Loops.py
+++ HackerRank/04-Class-V/05-Loops.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
     print("Input no")
     n = int(input())
 for i in range(1, 11):
-    print(str(n) + " x " + str(i) + " = " + sr(n * i))
+    print(str(n) + " x " + str(i) + " = " + str(n * i))
