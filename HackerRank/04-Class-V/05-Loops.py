97fa3b50e1efaa4cf06a5c30f5f1a92796208fc7
unknown <koushith97@gmail.com>
2019-07-21 11:44:20 +0530

    day4

--- HackerRank/04-Class-V/05-Loops.py
+++ HackerRank/04-Class-V/05-Loops.py
@@ -18,5 +18,5 @@ import sys
 if __name__ == '__main__':
     print("Input no")
     n = int(input())
-for i in range(1 11):
+for i in range(1, 11):
     print(str(n) + " x " + str(i) + " = " + str(n * i))
